6af81e18b7e65c957a34f700ef7e61f4409ac3d8
euroland3 <euroland3@EUROLAND3-PC>
2017-12-26 11:22:41 +0700

    Add project files.

---
+++ HauiUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HauiUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
